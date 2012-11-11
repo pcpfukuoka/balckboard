3f96dbd5c4b62241a4d0ac5a5c3c6c15bf40a6f6
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-11-11 18:24:22 +0900

    黒板ｊｓ
    テスト

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -15,8 +15,8 @@ onAppReady(function(param) {
 		clearTimeout(imgLoadTimer);
 		imgLoadTimer = null;
 	}, 2000);
-
-	var canvas = $("#canvas");
+	var canvas = document.getElementById("canvas");
+	//var canvas = $("#canvas");
 	var ctx = canvas[0].getContext("2d");
 	var loadedImages = param.loadedImages;
 	var LINE_PATTERNS = {
@@ -403,12 +403,12 @@ onAppReady(function(param) {
 		true)
 	});
 	$("#next").click(function(e){
-
+		if(canvas.getcontext){
 			var img= new Image();
 			console.log("next");
 			var type ='image/jpeg';
-			img.src=ctx.toDataURL(type);
-
+			img.src=canvas.toDataURL(type);
+		}
 	});
 	/**
 	 * ここより下はサーバにデータを送る処理
