c9e15d23c9bc01bc8d152b074f87fb5949bfcf73
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-11-11 20:44:59 +0900

    黒板ｊｓ
    テスト

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -19,12 +19,13 @@ onAppReady(function(param) {
 	//var ctx = canvas.getContext("2d");
 	var canvas = $("#canvas");
 	var ctx = canvas[0].getContext("2d");
-	console.log(ctx);
+	/////////////////////////////////////
+	/*				ここ				*/
+	/////////////////////////////////////
 	$("#next").click(function(e){
 		console.log("a");
-
 			var img=new Image();
-			console.log("next");
+			console.log("next");d
 			img.src=canvas.toDataURL();
 
 	});
