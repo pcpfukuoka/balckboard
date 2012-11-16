d64979f7322e7179ca6c73931b6807c3bba1ed3a
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-11-17 01:19:30 +0900

    あ
    あ

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -247,10 +247,10 @@ onAppReady(function(param) {
 
 				}
 
-			else if(param.x = "保存" && share != "true")
+			else if(param.x == "保存" && share != "true")
 			{
-				console.log("param.x");
-				console.log("param.color");
+				console.log(param.x);
+				console.log(param.color);
 				var canvas = document.getElementById("canvas");  //canvas要素を取得
 				var can = canvas.getContext('2d');
 				var img01 = new Image();
