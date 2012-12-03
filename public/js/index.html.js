815efcec4a2cb6aa281e83d49ed5cecd5d8a9e80
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-03 12:28:54 +0900

    a
    a

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -616,7 +616,7 @@ onAppReady(function(param) {
 			fn(command.param);
 		}
 		socket.on('page_jump',function(){
-			document.location = "http://49.212.201.99/pcp2012";
+			document.location = "http://49.212.201.99/pcp2012/index.php";
 		});
 		socket.on('connect', function(commands) {
 			$('#loadingMessage').hide();
