30eabab546ee04e7165820862b68f260589e3f46
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-12 00:54:49 +0900

    黒板
    logてｓｔ

--- server.js
+++ server.js
@@ -261,6 +261,7 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 			  //結果用
 			  .on('result', function(rows) {
 
+				  socket.emit('log_test',page_move);
 				  //現在のページ数を格納
 				  var max_page = rows['page_num'];
 				  //現在表示しているページにカーソルをそろえる
@@ -275,7 +276,6 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 
 				 var sql2 = 'UPDATE board SET div_url = "'+ command.param.start.y + '", canvas_url = "'+ command.param.end.x +'" WHERE date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = '+ now_page + ';';
 
-				 socket.emit('log_test',sql2);
 				 var query2 = connection.query(sql2);
 					query2
 					  //エラーログ
@@ -290,7 +290,6 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 
 
 						var sql3 = 'SELECT * FROM board WHERE date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = "'+ now_page + '";';
-						socket.emit('log_test',sql3);
 
 						var query3 = connection.query(sql3);
 						query3
