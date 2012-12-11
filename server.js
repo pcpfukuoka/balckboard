6bea5a74afe49cade83ab066db9414855adac8f9
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2012-12-12 01:15:04 +0900

    a
    a

--- server.js
+++ server.js
@@ -178,8 +178,11 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 
 				  //現在のページ数を格納
 				  var max_page = rows['page_num'];
-				  var now_page = max_page -(max_page- command.param.start.x - 1);
-				  page_move= 0;
+
+				  var aaa= max_page-page_move -1;
+				  var now_page = max_page -aaa;
+
+				  page_move= max_page + 1;
 
 				  var sql2 = 'UPDATE board SET div_url = "'+ command.param.start.y + '", canvas_url = "'+ command.param.end.x +'" WHERE date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = '+ now_page + ';';
 
