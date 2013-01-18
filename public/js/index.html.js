5f8e4e0440bca357942117b1f004f461f6f5ef4d
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-18 11:21:53 +0900

    授業
    エラー訂正

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -52,7 +52,7 @@ onAppReady(function(param) {
 		$("#button").append(e);
 
 		//クラステーブルを生成する処理
-		$.post('php/student_list.php', {
+		$.post('../php/student_list.php', {
 	        id : group_seq
 	    },
 	    //戻り値として、user_seq受け取る
