12ea165fc954e0fc78468fe1d9082ba20220cbc1
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-22 14:45:09 +0900

    授業
    エラー

--- server.js
+++ server.js
@@ -285,7 +285,6 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 					}
 
 				 var sql2 = 'UPDATE board SET div_url = "'+ command.param.start.y + '", canvas_url = "'+ command.param.end.x +'" WHERE end_flg="1" AND  date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = '+ now_page + ';';
-
 				 var query2 = connection.query(sql2);
 					query2
 						//エラーログ
@@ -388,7 +387,7 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 						 var aaa= max_page-page_move -1;
 						 now_page = max_page -aaa;
 
-						 var sql3 = 'SELECT * FROM board WHERE AND end_flg="1" AND date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = "'+ now_page + '";';
+						 var sql3 = 'SELECT * FROM board WHERE end_flg="1" AND date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "15" AND subject_seq = "15" AND page_num = "'+ now_page + '";';
 
 						 var query3 = connection.query(sql3);
 						 query3
