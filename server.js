89941d204de669fa558310681026ff4fad14efec
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-01-24 11:39:53 +0900

    授業
    エラー

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ var sockets = io.of('/chalkboard').on('connection', function(socket) {
 
 
 		//end_flagを１にして使用させないようにする
-		var sql = 'UPDATE board SET end_flg = "2" WHERE date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "'+command.param.group_seq+'" AND subject_seq = "'+command.param.subject+'" AND end_flg="1";';
+		var sql = 'UPDATE board SET end_flg = "2" WHERE date = DATE_FORMAT(now(),"%Y-%m-%d") AND class_seq = "'+command.param.y.group_seq+'" AND subject_seq = "'+command.param.y.subject+'" AND end_flg="1";';
 
 		var query = connection.query(sql);
 		query
