2b5196d85d6ff79b99fed602f5e31e620f9c1868
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-02-03 11:47:14 +0900

    授業
    テーブルふぃある

---
+++ public/php/student_autho.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<script src="lib/js/modernizr-2.0.6.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
+		<script src="../js/student_autho_js.js"></script>
+	</head>
+	<body onload="table()">
+	</body>
+</html>
