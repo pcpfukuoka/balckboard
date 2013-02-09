3a0ced63a9059d350494e82c251542495ed6de03
tominagakeisuke <o_hara1145015@mail.goo.ne.jp>
2013-02-09 15:36:59 +0900

    授業
    テスト

--- public/js/index.html.js
+++ public/js/index.html.js
@@ -382,12 +382,20 @@ onAppReady(function(param) {
 				var can = canvas.getContext('2d');
 
 
-				/* divのＵＲＬの変更 */
-				var div = document.getElementById("chalkboard");
-				div.style.background = param.end.x;
 
 
+				$('#chalkboardAndOthers').empty();
+		        //黒板をまずなくし、新規で黒板のタグを作成する
+		        var tag='<div id="chalkboard" style="background: '+param.end.x+';background-repeat:no-repeat">'
+		            +'<canvas id="canvas" width="900" height="700">'
+		            +'Your browser is not supported. Use modern browser (e.g. IE9 or later).'
+		            +'</canvas>'
+		            +'</div>';
+		        $('#chalkboardAndOthers').append(tag);
 
+
+		        var canvas = document.getElementById("canvas");
+				var can = canvas.getContext('2d');
 
 				/* canvasに画像の描画 */
  				var img1 = new Image();
